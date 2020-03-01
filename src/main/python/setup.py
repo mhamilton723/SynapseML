b1a383efd1a39c92254e91988c0e71aa87de0e00
Daniel Ciborowski <dciborow@microsoft.com>
2020-03-01 02:50:56 -0500

    Update setup.py

--- src/main/python/setup.py
+++ src/main/python/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dciborowMMLSpark",
-    version="0.0.28",
+    version="0.0.29",
     description="Microsoft ML for Spark",
     long_description="Microsoft ML for Apache Spark contains Microsoft's open source " +
                      "contributions to the Apache Spark ecosystem",
